a2e45d44ae6fa593e2fed9a40a93c886a5579f66
Vudutha Venkata Surya Kiran <67712163+venkatasurya777@users.noreply.github.com>
2021-04-11 00:29:45 +0530

    Add files via upload

---
+++ src/Spinner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Spinner = (props) => {
+  return (
+    <div className="ui active dimmer">
+      <div className="ui text loader">{props.message || "Loading...."}</div>
+    </div>
+  );
+};
+
+export default Spinner;
